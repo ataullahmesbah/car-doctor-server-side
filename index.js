600a73cccc23b449afcf4ede0d2bf40f16b007a7
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-13 01:16:42 +0600

    server data confirm

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
         const id = req.params.id;
 
         const options = {
-            projection: { title: 1, price: 1, service_id: 1 },
+            projection: { title: 1, price: 1, service_id: 1, img: 1 },
         }
         const query = { _id: new ObjectId(id) }
         const result = await serviceCollection.findOne(query, options);
@@ -51,9 +51,47 @@ async function run() {
     })
 
     // bookings
+    app.get('/bookings', async(req, res) => {
+        let query = {};
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
 
     app.post('/bookings', async (req, res) => {
         const booking = req.body;
+        console.log(booking);
+        // mongodb data send
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+    })
+
+    // update
+
+    app.patch('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updateBookings = req.body;
+        console.log(updateBookings);
+
+        const updateDoc = {
+            $set: {
+                status: updateBookings.status
+            }
+        }
+        const result = await bookingCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    })
+
+    //specific delete id
+
+    app.delete('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result)
     })
 
 
